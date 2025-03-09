760897b8ce48a9e2780e08e48c7ff4a298f0782d
doanvu2000 <dauphong13@gmail.com>
2025-03-09 15:51:21 +0700

    add hilt, dagger, ksp libs

--- app/src/main/java/com/jin/notemanagerjc/MainActivity.kt
+++ app/src/main/java/com/jin/notemanagerjc/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.jin.notemanagerjc.ui.theme.NoteManagerJCTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +37,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteManagerJCTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    NoteManagerJCTheme {
+//        Greeting("Android")
+//    }
+//}
